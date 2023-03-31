2a0b8a3f82b73ed2c2ede296280f018734adb425
Ian Halliday <ianwjhalliday@gmail.com>
2023-03-31 14:49:37 -0400

    Comment out all rcsid globals to silence -Wunused-const-variable

--- linuxdoom-1.10/i_video.c
+++ linuxdoom-1.10/i_video.c
@@ -86,8 +86,8 @@ void I_InitGraphics(void)
 
 #if 0
 
-static const char
-rcsid[] = "$Id: i_x.c,v 1.6 1997/02/03 22:45:10 b1 Exp $";
+// static const char
+// rcsid[] = "$Id: i_x.c,v 1.6 1997/02/03 22:45:10 b1 Exp $";
 
 #include <stdlib.h>
 #include <unistd.h>
